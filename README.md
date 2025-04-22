3689fbca91f6c83675c28dc273f9da38d49fa637
Alqama Hasnain <alqama.hasnain@gmail.com>
2025-04-22 11:46:50 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# ollama-chat" 
+
+very basic app, allows to chat with ollama.
+Pending UI dev
--- obj/Debug/net9.0-windows/MyWpfApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/MyWpfApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWpfApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a747027433d54886c700aa2288195d9802ed01")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWpfApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWpfApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
